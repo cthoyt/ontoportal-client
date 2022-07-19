e77535c3340194b919c1385f50377fdc9a22d57e
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-07-19 13:44:26 -0400

    Make generic

--- tests/cases.py
+++ tests/cases.py
@@ -5,26 +5,21 @@
 import unittest
 from typing import ClassVar, Type
 
+import unittest_templates
+
 from ontoportal_client import OntoPortalClient
 
 __all__ = ["TestOntoPortalClient"]
 
 
-class TestOntoPortalClient(unittest.TestCase):
+class TestOntoPortalClient(unittest_templates.GenericTestCase[OntoPortalClient]):
     """Test a client."""
 
-    client_cls: ClassVar[Type[OntoPortalClient]]
-    client: ClassVar[OntoPortalClient]
     test_acronym: ClassVar[str]
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        """Set up the class with an OntoPortal client."""
-        cls.client = cls.client_cls()
 
     def test_get_ontologies(self):
         """Test getting the ontologies."""
-        res = self.client.get_ontologies()
+        res = self.instance.get_ontologies()
         self.assertLessEqual(0, len(res))
         acronyms = {entry["acronym"] for entry in res}
         self.assertIn(self.test_acronym, acronyms)
--- tests/test_client.py
+++ tests/test_client.py
@@ -4,13 +4,14 @@
 
 import unittest_templates
 
-from ontoportal_client import (
+from ontoportal_client.api import (
     AgroPortalClient,
     BioPortalClient,
     EcoPortalClient,
     MatPortalClient,
     MedPortalClient,
     OntoPortalClient,
+    PreconfiguredOntoPortalClient,
     SIFRBioPortalClient,
 )
 from tests import cases
@@ -19,42 +20,42 @@ from tests import cases
 class TestBioPortalClient(cases.TestOntoPortalClient):
     """Test the BioPortal client."""
 
-    client_cls = BioPortalClient
+    cls = BioPortalClient
     test_acronym = "GO"
 
 
 class TestEcoPortalClient(cases.TestOntoPortalClient):
     """Test the EcoPortal client."""
 
-    client_cls = EcoPortalClient
-    test_acronym = "ENVO"
+    cls = EcoPortalClient
+    test_acronym = "AGROVOC"
 
 
 class TestAgroPortalClient(cases.TestOntoPortalClient):
     """Test the AgroPortal client."""
 
-    client_cls = AgroPortalClient
+    cls = AgroPortalClient
     test_acronym = "AGROVOC"
 
 
 class TestMatPortalClient(cases.TestOntoPortalClient):
     """Test the MatPortal client."""
 
-    client_cls = MatPortalClient
+    cls = MatPortalClient
     test_acronym = "DISO"
 
 
-class SIFRBioPortalClient(cases.TestOntoPortalClient):
+class TestSIFRBioPortalClient(cases.TestOntoPortalClient):
     """Test the SIFR BioPortal client."""
 
-    client_cls = SIFRBioPortalClient
+    cls = SIFRBioPortalClient
     test_acronym = "NABM"
 
 
 class TestMedPortalClient(cases.TestOntoPortalClient):
     """Test the MedPortal client."""
 
-    client_cls = MedPortalClient
+    cls = MedPortalClient
     test_acronym = "DOID"
 
 
@@ -63,3 +64,6 @@ class TestClients(unittest_templates.MetaTestCase[OntoPortalClient]):
 
     base_cls = OntoPortalClient
     base_test = cases.TestOntoPortalClient
+    skip_cls = {
+        PreconfiguredOntoPortalClient,
+    }
