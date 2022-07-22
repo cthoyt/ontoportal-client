e12baced104d0364ed5e19d688bf8b37c1495aa3
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-07-21 20:27:05 -0400

    Pass flake8

--- src/ontoportal_client/api.py
+++ src/ontoportal_client/api.py
@@ -10,7 +10,7 @@ from typing import Any, ClassVar, Dict, Optional
 import pystow
 import requests
 
-from .constants import NAMES, URLS
+from .constants import URLS
 
 __all__ = [
     # Base clients
